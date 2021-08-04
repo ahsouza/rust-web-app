22e5558576d22fd0a35f1d21091daf3b00a43168
ahsouza <annibalhsouza@gmail.com>
2021-08-04 16:24:47 -0300

    feat: implement rocket routes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn home() -> &'static str {
+  "Welcome to Sweet Home "
+}
+
+#[get("/properties/<title>")]
+fn GetProperty(title: &str) -> String {
+  format!("Your property is available for sale, soon we will have news from {}!", title)
+}
+
+#[launch]
+fn rocket() -> _ {
+  rocket::build()
+    .mount("/", routes![home])
+    .mount("/", routes![GetProperty])
 }
